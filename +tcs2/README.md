0c4a9ec4c52dba51888ab85944f197662e1e1843
cedlenoir <cedric.lenoir@uclouvain.be>
2025-01-15 10:18:35 +0100

    clean repo, duplicates of tcs2 library

--- +tcs2/README.md
+++
@@ -1,2 +0,0 @@
-# TCS2 functions package
-version 2.0.2
--- +tcs2/get_serial.m
+++
@@ -1,7 +0,0 @@
-function obj = get_serial
-% get_serial  Get serial object.
-%    COM = tcs2.get_serial
-
-global TCS2_SERIAL
-
-obj = TCS2_SERIAL;
